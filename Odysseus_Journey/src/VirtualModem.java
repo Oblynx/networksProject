62ae5d621d5d9e907fa7809eeffd741d9d279ec2
Konstantinos Samaras-Tsakiris <kisamara@ece.auth.gr>
2015-12-08 17:21:27 +0200

    Started image acq

--- Odysseus_Journey/src/VirtualModem.java
+++ Odysseus_Journey/src/VirtualModem.java
@@ -1,4 +1,5 @@
 import ithakimodem.Modem;
+import java.nio.*;
 
 public class VirtualModem {
 	public void RXsetup(int speed, int timeout){
@@ -50,7 +51,47 @@ public class VirtualModem {
 	}
 
 	public void imageRX(String code){
+		ByteBuffer sigStart= new ByteBuffer(), sigEnd= new ByteBuffer(), packet= new ByteBuffer();
+		// perhaps bytestream?
+		ByteFlag mdk= new ByteFlag();
+		Boolean packetStart= false, packetIn= false, packetEnd= false;
+		mdk.terminate= false;
 		
+		modem.write(code.getBytes());
+		while(!mdk.terminate){
+			mdk= readByte();
+			if (!mdk.terminate){
+				// Update packet delimiter buffers
+				if(sigStart.length() < 6) sigStart.append((char)mdk.k);
+				else {
+					sigStart.deleteCharAt(0);
+					sigStart.append((char)mdk.k);
+				}
+				if(sigEnd.length() < 5) sigEnd.append((char)mdk.k);
+				else {
+					sigEnd.deleteCharAt(0);
+					sigEnd.append((char)mdk.k);
+				}
+				// Signal accordingly
+				Boolean oldpacketStart= packetStart, oldpacketEnd= packetEnd;
+				packetStart= sigStart.toString().equals("PSTART");
+				packetEnd= sigEnd.toString().equals("PSTOP");
+				// On packet start...
+				if (!oldpacketStart && packetStart){
+					packetIn= true;
+					packet.setLength(0);
+					packet.append(sigStart.deleteCharAt(6-1).toString());
+				}
+				// While in packet transmission
+				if (packetIn) packet.append((char)mdk.k);
+				// On packet end
+				if (!oldpacketEnd && packetEnd){
+					packetIn= false;
+					processEchoPacket(packet);
+					modem.write(code.getBytes());
+				}
+			}
+		}
 	}
 	
 	public void close(){ modem.close(); }
