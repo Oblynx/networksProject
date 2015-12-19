8d37c9a9ffa4732f5ff919d818caa87fdb84c83e
Konstantinos Samaras-Tsakiris <kisamara@auth.gr>
2015-12-19 19:24:39 +0200

    Implement ARQ

--- Odysseus_Journey/src/VirtualModem.java
+++ Odysseus_Journey/src/VirtualModem.java
@@ -3,6 +3,7 @@ import ithakimodem.Modem;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.file.*;
+import java.time.Instant;
 import java.util.*;
 
 public class VirtualModem {
@@ -32,10 +33,33 @@ public class VirtualModem {
 	}
 	public Packet gpsRX(String code){
 		System.out.println("GPS receiving");
-		Packet packet= getPacket(code, gpsStart, gpsEnd, );
+		Packet packet= getPacket(code, gpsStart, gpsEnd, 100);
 		System.out.println("GPS RECEIVED!");
 		processGPS(packet);
+		ArrayList<Byte> empty= new ArrayList<Byte>();
+		System.out.println("GPS addendum...");
+		packet= getPacket("",empty,empty,100);
+		System.out.println("GPS addendum RECEIVED!");
+		for(byte b: packet.data)
+			System.out.print((char)b);
 		return packet;
+	}
+	public ArrayList<Integer> arqRX(String ack, String nack, long durationMillis){
+		ArrayList<Integer> retries= new ArrayList<Integer>();
+		long startTime= System.currentTimeMillis();
+		while(System.currentTimeMillis()-startTime < durationMillis){
+			int retry= 0;
+			Packet packet= getPacket(ack, echoStart, echoEnd, 100);
+			//If transmission error, resend
+			while (errorARQ(packet)){
+				packet= getPacket(nack, echoStart, echoEnd, 100);
+				retry++;
+			}
+			retries.add(retry);
+			//Got correct package
+			processARQ(packet, retry);
+		}
+		return retries;
 	}
 	public void close(){ modem.close(); }
 	
@@ -51,8 +75,46 @@ public class VirtualModem {
 		image= packet.data.toArray(image);
 		try { Files.write(path, toPrimitives(image), StandardOpenOption.CREATE);
 		} catch (IOException e) { e.printStackTrace(); }
-		System.out.println("Image saved to file.");
+		System.out.println("Image saved to file#"+index+". Timestamp: "+Instant.now());
 	}
+	private void processGPS(Packet packet){
+		StringBuffer output= new StringBuffer();
+		int j=36;
+		for(byte b: packet.data){
+			if (j==0) break;
+			output.append((char)b);
+			if (((char)b) == '\n') j--;
+		}
+		System.out.print(output);
+		System.out.println("\t--> GPS END <--");
+	}
+	private boolean errorARQ(Packet packet){
+		StringBuffer fcsBuf= new StringBuffer();
+		byte[] hex= new byte[16];
+		if(packet.size() != 58){
+			System.out.println("Error: packet size= "+packet.size());
+			throw new RuntimeException();
+		}
+		//<31bytes>HEX FCS<6bytes> 
+		for(int i=31; i< 31+16; i++) hex[i-31]= packet.data.get(i);
+		for(int i=49; i<52; i++) fcsBuf.append( (char) ((byte)packet.data.get(i)) );	
+		//Parse fcs
+		int fcs= Integer.parseInt(fcsBuf.toString());
+		//Calculate HEX xor
+		int fcsCheck= fcs(hex);
+			//System.out.println("\nCalculated FCS: "+fcsCheck);
+		return fcs != fcsCheck;
+	}
+	public int fcs(byte[] hex){
+		int fcsCheck= (int)hex[0];
+		for(int i=1; i<16; i++) fcsCheck^= (int)hex[i];
+		return fcsCheck;
+	}
+	private void processARQ(Packet packet, int retries){
+		processEchoPacket(packet);
+		System.out.println("\t--> Retries= "+retries);
+	}
+	
 	private Packet getPacket(String code, ArrayList<Byte> start, ArrayList<Byte> end, int capacity){
 		ArrayList<Byte> sigStart= new ArrayList<Byte>(), sigEnd= new ArrayList<Byte>();
 		ByteFlag mdk= new ByteFlag();
@@ -63,35 +125,38 @@ public class VirtualModem {
 		while(!mdk.terminate){
 			mdk= readByte();
 			if (!mdk.terminate){
-				// Update packet delimiter buffer
-				if(sigStart.size() < start.size()) sigStart.add((byte)mdk.k);
-				else {
-					sigStart.remove(0);
-					sigStart.add((byte)mdk.k);
-				}
-				if(sigEnd.size() < end.size()) sigEnd.add((byte)mdk.k);
-				else {
-					sigEnd.remove(0);
-					sigEnd.add((byte)mdk.k);
-				}
-				// Signal accordingly
-				boolean oldpacketStart= packetStart, oldpacketEnd= packetEnd;
-				packetStart= sigStart.equals(start);
-				packetEnd= sigEnd.equals(end);
-				// On packet start...
-				if (!oldpacketStart && packetStart){
-					packetIn= true; 
-					curPack.startTime= System.currentTimeMillis();
-					curPack.data.ensureCapacity(capacity);
-					for(int i=0; i<start.size()-1; i++) curPack.data.add(start.get(i));
-				}
-				// While in packet transmission
-				if (packetIn) curPack.data.add((byte)mdk.k);
-				// On packet end
-				if (!oldpacketEnd && packetEnd){
-					packetIn= false;
-					curPack.endTime= System.currentTimeMillis();
-					mdk.terminate= true;
+				if(start.size() == 0) curPack.data.add((byte)mdk.k);
+				else{
+					// Update packet delimiter buffer
+					if(sigStart.size() < start.size()) sigStart.add((byte)mdk.k);
+					else {
+						sigStart.remove(0);
+						sigStart.add((byte)mdk.k);
+					}
+					if(sigEnd.size() < end.size()) sigEnd.add((byte)mdk.k);
+					else {
+						sigEnd.remove(0);
+						sigEnd.add((byte)mdk.k);
+					}
+					// Signal accordingly
+					boolean oldpacketStart= packetStart, oldpacketEnd= packetEnd;
+					packetStart= sigStart.equals(start);
+					packetEnd= sigEnd.equals(end);
+					// On packet start...
+					if (!oldpacketStart && packetStart){
+						packetIn= true; 
+						curPack.startTime= System.currentTimeMillis();
+						curPack.data.ensureCapacity(capacity);
+						for(int i=0; i<start.size()-1; i++) curPack.data.add(start.get(i));
+					}
+					// While in packet transmission
+					if (packetIn) curPack.data.add((byte)mdk.k);
+					// On packet end
+					if (!oldpacketEnd && packetEnd){
+						packetIn= false;
+						curPack.endTime= System.currentTimeMillis();
+						mdk.terminate= true;
+					}
 				}
 			}
 		}
@@ -126,7 +191,15 @@ public class VirtualModem {
 	private ArrayList<Byte> echoStart= new ArrayList<Byte>() {{ try{ for(byte b: "PSTART".getBytes("US-ASCII")) add(b); }
 																catch (UnsupportedEncodingException e) {e.printStackTrace();} }};
 	@SuppressWarnings("serial")
-	private ArrayList<Byte> echoEnd= new ArrayList<Byte>()   {{ try{ for(byte b: "PSTOP".getBytes("US-ASCII")) add(b);  }
+	private ArrayList<Byte> echoEnd= new ArrayList<Byte>()   {{ try{ for(byte b: "PSTOP".getBytes("US-ASCII")) add(b); }
 																catch (UnsupportedEncodingException e) {e.printStackTrace();} }};
+	@SuppressWarnings("serial")
+	private ArrayList<Byte> gpsStart= new ArrayList<Byte>()
+		{{ try{ for(byte b: "START ITHAKI GPS TRACKING\r\n".getBytes("US-ASCII")) add(b); }
+		   catch (UnsupportedEncodingException e) {e.printStackTrace();} }};
+	@SuppressWarnings("serial")
+	private ArrayList<Byte> gpsEnd= new ArrayList<Byte>()
+		{{ try{ for(byte b: "STOP ITHAKI GPS TRACKING\r\n".getBytes("US-ASCII")) add(b); }
+		   catch (UnsupportedEncodingException e) {e.printStackTrace();} }};
 	private Modem modem;
 }
--- Odysseus_Journey/src/userApplication.java
+++ Odysseus_Journey/src/userApplication.java
@@ -3,11 +3,12 @@ public class userApplication {
 	public static void main(String[] param){
 		VirtualModem vm= new VirtualModem();
 		vm.RXsetup(8000, 500000);
-		vm.echoPacketRX("E2063\r",2000);
-		//vm.imageRX("M8649\rCAM=5\rSIZE=S\r");
-		vm.imageRX("M8649\r", 1);
-		vm.RXsetup(8000, 500000);
-		vm.imageRX("G5741\r", 2);
+		vm.echoPacketRX("TEST\r",100);
+		//vm.imageRX("M8854 CAM=PTZ\r", 7);
+		//vm.imageRX("M8649\r", 1);
+		//vm.imageRX("G5741\r", 2);
+		//vm.gpsRX("P3613 T=403725225726\r");
+		vm.arqRX("Q6268\r", "R5031\r", 1000);
 		vm.close();
 	}
 }
