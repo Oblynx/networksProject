21cd03c5b53766b2737aa77c7f0f50298535cb36
Konstantinos Samaras-Tsakiris <kisamara@auth.gr>
2015-12-23 07:30:46 +0200

    Complete report

--- Odysseus_Journey/analysis/analysis.m
+++ Odysseus_Journey/analysis/analysis.m
@@ -21,13 +21,13 @@ reactTimeARQ= arques9sp8000(:,3);
 retries= arques9sp8000(:,4);
 clear('echo*','arqu*');
 %}
-%% Get data 11
-load('log12_sp80000.mat');
-transferTimeARQ= arques12(:,2)-arques12(:,1);
-transferTimeECH= echoes12(:,2)-echoes12(:,1);
-reactTimeECH= echoes12(:,3);
-reactTimeARQ= arques12(:,3);
-retries= arques12(:,4);
+%% Get data 12+
+load('log13_sp80000.mat');
+transferTimeARQ= arques(:,2)-arques(:,1);
+transferTimeECH= echoes(:,2)-echoes(:,1);
+reactTimeECH= echoes(:,3);
+reactTimeARQ= arques(:,3);
+retries= arques(:,4);
 clear('echo*','arqu*');
 %% remove total outliers
 %{
@@ -42,8 +42,8 @@ reactTimeARQ= sort(reactTimeARQ);
 reactTimeARQ= reactTimeARQ(1:end-outliers);
 retries= sort(retries);
 retries= retries(1:end-outliers);
-reactTime= [reactTimeECH;reactTimeARQ];
 %}
+reactTime= [reactTimeECH;reactTimeARQ];
 %% Histograms
 figure(1);
 logHist(reactTime);
@@ -53,30 +53,30 @@ grid minor;
 %
 figure(2);
 hist(transferTimeECH,length(unique(transferTimeECH)));
-title('G1: E6996, 21/12, ~23:00 -- ECHO packet transfer time');
+title('G1: E0364, 23/12, 6:15 -- ECHO packet transfer time');
 xlabel('t(ms)');
 grid minor;
 figure(3);
 %hist(transferTimeARQ,sqrt(length(transferTimeARQ)));
 logHist(transferTimeARQ);
-title('G2: Q4761-R7581, 21/12, ~23:00 -- ARQ packet transfer time');
+title('G2: Q5604-R2648, 23/12, 6:15 -- ARQ packet transfer time');
 xlabel('t(ms)');
 grid minor;
-%
+%{
 figure(4);
 logHist(transferTimeECH+reactTimeECH);
-title('G1: E6996, 21/12, ~23:00 -- ECHO packet total transfer time');
+title('G1: E0364, 23/12, 6:15 -- ECHO packet total transfer time');
 xlabel('t(ms)');
 grid minor;
 figure(5);
 logHist(transferTimeARQ+reactTimeARQ);
-title('G2: Q4761-R7581, 21/12, ~23:00 -- ARQ packet total transfer time');
+title('G2: Q5604-R2648, 23/12, 6:15 -- ARQ packet total transfer time');
 xlabel('t(ms)');
 grid minor;
-%
+%}
 figure(6);
 [f,x]=hist(retries,length(unique(retries)));
 bar(unique(retries),f);
-title('G3: Q4761-R7581, 21/12, ~23:00 -- Retries per packet');
+title('G3: Q5604-R2648, 23/12, 6:15 -- Retries per packet');
 grid minor;
 %}
